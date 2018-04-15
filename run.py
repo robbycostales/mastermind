bff8f570d9bf0401a29255a5246e0c2694b3aedf
ROBBY <robbycostales@gmail.com>
2018-04-15 14:56:57 -0400

    solved

--- run.py
+++ run.py
@@ -1,9 +1,11 @@
 from __future__ import print_function
 from __future__ import division  # this ensures the / operation give floats by default
 import itertools
+import random
+from collections import Counter
 
 
-def evaluate_guess(guess):
+def evaluate_guess(guess, c=None):
     '''
     This function compares a guess and a code and returns (numCorrect,numTransposed)
     which correspond to the number of black and white pegs returned in Mastermind.
@@ -11,19 +13,22 @@ def evaluate_guess(guess):
     '''
     global code
 
+    if c == None:
+        c = code
+
 	# Get the length n of the guess and the code.
-    assert(len(guess) == len(code))
+    assert(len(guess) == len(c))
     n = len(guess)
 
     # Determine the correct and incorrect positions.
-    correct_positions = [i for i in list(range(n)) if guess[i] == code[i]]
-    incorrect_positions = [i for i in list(range(n)) if guess[i] != code[i]]
+    correct_positions = [i for i in list(range(n)) if guess[i] == c[i]]
+    incorrect_positions = [i for i in list(range(n)) if guess[i] != c[i]]
     num_correct = len(correct_positions)
 
     # Reduce the guess and the code by removing the correct positions.
     # Create the set values that are common between the two reduced lists.
     reduced_guess = [guess[i] for i in incorrect_positions]
-    reduced_code = [code[i] for i in incorrect_positions]
+    reduced_code = [c[i] for i in incorrect_positions]
     reduced_set = set(reduced_guess) & set(reduced_guess)
 
     # Determine the number of transposed values.
@@ -39,17 +44,53 @@ def KnuthFive():
     Implement Knuth's algorithm here.
     You cannot use the global variable code.
     '''
+
+
+    S = [tuple(x) for x in itertools.product(range(1,7), repeat=4)]
+    R = [tuple(x) for x in itertools.product(range(1,7), repeat=4)]
+
     guess = (1,1,2,2)
+    R.remove(guess)
     guessList = [guess]
     num_correct, num_transposed = evaluate_guess(guess)
 
+    while num_correct != 4:
+        # remove impossible codes
+        newS = []
+        for s in S:
+            if evaluate_guess(guess, s) == (num_correct, num_transposed):
+                newS.append(s)
+            else:
+                continue
+        S = newS
+
+        if len(S) == 1:
+            guessList.append(S[0])
+            break
+
+        # find next guess with maximum score
+        # possible guesses
+        scores = []
+        for r in R:
+            r_hits = []
+            # code possibilities
+            for s in S:
+                x = evaluate_guess(r, s)
+                r_hits.append(x)
+
+            mode, mode_num = Counter(r_hits).most_common(1)[0]
+            score = len(S) - mode_num
+            scores.append(score)
+
+        max_score = max(scores)
+        guess = R[scores.index(max_score)]
+        guessList.append(guess)
+        num_correct, num_transposed = evaluate_guess(guess)
     return guessList
 
 
 if __name__ == "__main__":
     global code
-    
-
     # Create all 1296 possible Mastermind codes.
     allCodes = [tuple(x) for x in itertools.product(range(1,7), repeat=4)]
 
@@ -60,6 +101,7 @@ if __name__ == "__main__":
     totalGuesses = 0   # total number of guesses used to solve all solved codes
 
     for code in allCodes:
+        print("\n", code)
         guessList = KnuthFive()
         if guessList[-1] == code:
             totalSolved += 1
@@ -71,5 +113,5 @@ if __name__ == "__main__":
 
     print("total solved: %d" % totalSolved)
     print("worst case: %d" % worstCase)
-    print("average: %d" % (totalGuesses/totalSolved))
+    print("average: {}".format(totalGuesses/totalSolved))
     print("total unsolved: %d" % totalUnsolved)
---
+++ test.py
@@ -0,0 +1,3 @@
+import run
+
+print(run.evaluate_guess((1, 1, 1, 2), (1, 1, 1, 2)))
